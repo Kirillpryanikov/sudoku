9805930a03bec91f6f746c0882434c724bd34556
Kirillpryanikov <prianikovkirill@gmail.com>
2017-03-15 10:14:52 +0200

    Add github references & rebuild

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,7 @@ export default class App extends Component {
 	render() {
 		return (
 			<div>
+				Source code on <a href="https://github.com/Kirillpryanikov/sudoku" target="_blank">Github</a>
 				{this.props.children}
 			</div>
 		)
